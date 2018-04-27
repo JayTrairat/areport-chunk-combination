cada02e78cbe874292995a7daab2f8fb4e23d1fa
JayTrairat <codeoutpost@gmail.com>
2018-04-27 11:06:45 +0700

    feat(): cosine similarities result

--- chunk_combinator.py
+++ chunk_combinator.py
@@ -1,20 +1,16 @@
 def main():
     important_words = {}
     for index in range(1, 11):
-        with open('assets/important_words/important_words_pos_' + str(index) + '.txt', 'r', encoding='utf8') as source:
+        with open('areport-chunk-combination/assets/important_words/important_words_pos_' + str(index) + '.txt', 'r', encoding='utf8') as source:
             contents = [content.strip() for content in (source.readlines())]
             important_words[index] = contents
     list_of_name = [
-        pos_1 + pos_2 + pos_3 + pos_4 + pos_5 + pos_6
+        pos_1 + pos_2
         for pos_1 in important_words[1]
         for pos_2 in important_words[2]
-        for pos_3 in important_words[3]
-        for pos_4 in important_words[4]
-        for pos_5 in important_words[5]
-        for pos_6 in important_words[6]
     ]
 
-    with open('assets/naming_list_6_pos.txt', 'w', encoding='utf8') as outp:
+    with open('areport-chunk-combination/assets/naming_list_6_pos.txt', 'w', encoding='utf8') as outp:
         for item in list_of_name:
             outp.write(item)
             outp.write('\n')
